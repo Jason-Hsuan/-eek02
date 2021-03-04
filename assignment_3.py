9d0183613c0ca5d5c2ccc4b423fb096cc3f5f683
Jason-Hsuan <76401750+Jason-Hsuan@users.noreply.github.com>
2021-03-05 02:37:48 +0800

    Update assignment_3.py

--- assignment_3.py
+++ assignment_3.py
@@ -1,34 +1,19 @@
 def maxProduct(nums):
-  a = nums[0]
-  b = nums[0]
-  c = nums[0]
-  d = nums[0]
-  for i in nums:
-    if i > a and i > b:
-      b = a
-      a = i
-    elif i > b:
-      b = i
-  for j in nums:
-    if j < c and j < d:
-      d = c
-      c = j
-    elif j < d:
-      d=j
-  pos_max=a*b
-  neg_max=c*d
-  if pos_max > neg_max:
-    print(f"得到{pos_max} 因為 {a} 和 {b} 相乘得到最大值")
-  elif pos_max < neg_max:
-    print(f"得到{neg_max} 因為 {c} 和 {d} 相乘得到最大值")
-  else:
-    print(f"得到{pos_max} 因為 {a} 和 {b} 還有{c} 和 {d} 相乘得到最大值")
+    max_nums=nums[0]*nums[1]
+    for i in range(0,len(nums)):
+        for j in range(i,len(nums)-1):
+            if max_nums < nums[i]*nums[j+1]:
+                max_nums = nums[i]*nums[j+1]
+            j+=1
+        i+=1 
+    print(f"得到{max_nums}為最大值")   
 
-maxProduct([5, 20, 2, 20, 6, 10])
-maxProduct([5, 20, 2, 4, 6, 10])
+maxProduct([5, 20, 2, 10, 6, 10])
+maxProduct([5, 30, 2, 4, 6, 10])
 maxProduct([-1, 2, -10, 10, 20, 20])
 maxProduct([-1, 2, -10, -10, -20, 20])
 maxProduct([-1, 2, 20, 20, -20, -20])
+maxProduct([-1,20])
 
 
 
