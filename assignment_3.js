d9e428f5e260a8ca25148f818a189d43ddd89cc4
Jason-Hsuan <76401750+Jason-Hsuan@users.noreply.github.com>
2021-03-05 02:37:16 +0800

    Update assignment_3.js

--- assignment_3.js
+++ assignment_3.js
@@ -1,40 +1,20 @@
+//0x1,0x2,0x3,0x4,..
+//1x2,1x3,1x4...
+//2x3,2x4....
 function maxProduct(nums){
-    var a = nums[0];
-    var b = nums[0];
-    var c = nums[0];
-    var d = nums[0];
-    for(var i = 0; i <=nums.length; i++){
-        if(nums[i] > a && nums[i] > b){
-            b = a;
-            a = nums[i];
-        }
-        else if (nums[i] > b){
-            b = nums[i];
-        } 
-    }
-    for(var j = 0; j <=nums.length; j++){
-        if(nums[j] < c && nums[j] < d){
-            d = c;
-            c = nums[j];
-        }
-        else if (nums[j] < d){
-            d = nums[j];
-        } 
-    }
-    var pos_max=a*b;
-    var neg_max=c*d;
-    if(pos_max > neg_max){
-        console.log(`得到${pos_max} 因為 ${a} 和 ${b} 相乘得到最大值`);
-        }
-    else if(pos_max < neg_max){
-        console.log(`得到${neg_max} 因為 ${c} 和 ${d} 相乘得到最大值`);
-        }
-    else{
-        console.log(`得到${pos_max} 因為 ${a} 和 ${b} 還有${c} 和 ${d} 相乘得到最大值`);
-        }
+  var max_nums=nums[0]*nums[1];
+  for(var i = 0; i <nums.length; i++){
+    for(var j = i; j <nums.length-1; j++){
+      if(max_nums < nums[i]*nums[j+1]){
+        max_nums = nums[i]*nums[j+1];
+    } 
+  }
+  }
+console.log(`得到${max_nums}為最大值`);
 }
-maxProduct([5, 20, 2, 20, 6, 10])
-maxProduct([5, 20, 2, 4, 6, 10])
+maxProduct([5, 20, 2, 10, 6, 10])
+maxProduct([5, 30, 2, 4, 6, 10])
 maxProduct([-1, 2, -10, 10, 20, 20])
 maxProduct([-1, 2, -10, -10, -20, 20])
 maxProduct([-1, 2, 20, 20, -20, -20])
+maxProduct([-1,20])
