77fc3a1a359cff8d56bf20ed22d669ad61de6d4a
Jason-Hsuan <76401750+Jason-Hsuan@users.noreply.github.com>
2021-03-06 10:22:07 +0800

    Update assignment_5.py

--- assignment_5.py
+++ assignment_5.py
@@ -14,15 +14,15 @@ def maxZeros(nums):
       else:
         n=0
   print(n_max)
-# 1:   nums[0]=1; 
-# 2:   nums[1]=0; n=1;
-# 3:   nums[2]=0; n=2;
-# 4:   nums[3]=0; n=3;
-# 5:   nums[4]=1; n=3; n_max=3;
-# 6:   nums[5]=1; n=3; n_max=3;
-# 7:   nums[6]=1; n=3; n_max=3;
-# 8:   nums[7]=0; n=1; n_max=3;
-# 9:   nums[8]=0; n=2; n_max=3;
+#1:   nums[0]=0; n=1;
+#2:   nums[1]=0; n=2;
+#3:   nums[2]=0; n=3; 
+#4:   nums[3]=1; n=0; n_max=3;
+#5:   nums[4]=0; n=1; n_max=3;
+#6:   nums[5]=0; n=2; n_max=3;
+#7:   nums[6]=1; n=0; n_max=3;
+#8:   nums[7]=0; n=1; n_max=3;
+#9:   nums[8]=0; n=2; n_max=3;
 
 maxZeros([0,1,0,0])
 maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0])
