69954f0bfa4d543f698d6e76cbe528001c75fabf
Jason-Hsuan <76401750+Jason-Hsuan@users.noreply.github.com>
2021-03-06 10:27:07 +0800

    Update assignment_5.js

--- assignment_5.js
+++ assignment_5.js
@@ -1,25 +1,38 @@
 function maxZeros(nums){
-    var n=0;
-    var n_max=0;
-    for(var i=0; i<nums.length; i++){
-      if(nums[i]==0){
-        n++;
-        if(i==nums.length-1){
-          if(n>n_max){
-            n_max=n;
-          }
-        }
-      }
-      else{
+  var n=0;
+  var n_max=0;
+  for(var i=0; i<nums.length; i++){
+    if(nums[i]==0){
+      n++;
+      if(i==nums.length-1){//8
         if(n>n_max){
           n_max=n;
-          n=0;
         }
       }
-  
     }
-    console.log(n_max)
+    else{
+      if(n>=n_max){
+        n_max=n;
+        n=0;
+      }
+      else{
+        n=0;
+      }
+    }
+//1:   nums[0]=0; n=1;
+//2:   nums[1]=0; n=2;
+//3:   nums[2]=0; n=3; 
+//4:   nums[3]=1; n=0; n_max=3;
+//5:   nums[4]=0; n=1; n_max=3;
+//6:   nums[5]=0; n=2; n_max=3;
+//7:   nums[6]=1; n=0; n_max=3;
+//8:   nums[7]=0; n=1; n_max=3;
+//9:   nums[8]=0; n=2; n_max=3;
   }
-  maxZeros([0,1,0,0])
-  maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0])
-  maxZeros([1, 1, 1, 1, 1])
+  console.log(n_max)
+}
+maxZeros([0,1,0,0])
+maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0])
+maxZeros([1, 1, 1, 1, 1])
+maxZeros([0,0,0,1,0,0,0,1,0])
+maxZeros([0,0,0,1,0,0,1,0,0])
